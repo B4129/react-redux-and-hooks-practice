3c4dc5625c09b0e8ba320a38592b74f1bb5cda7c
B4129 <B4129@oic.co.jp>
2021-10-06 10:05:15 +0900

    create template branch add : keisan

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from "react";
+
 export const App = () => {
-    return (
-        <div>this is a template for React app</div>
-    );
-}
+  const [count, setCount] = useState(0);
+  const reset = () => setCount(0);
+  const increment = () => setCount(count + 1);
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement = () => setCount(count - 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+  const double = () => setCount((previousCount) => previousCount * 2);
+  const divide3 = () =>
+    setCount((prevState) => (prevState % 3 === 0 ? prevState / 3 : prevState));
+  return (
+    <>
+      <div>count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={double}>x2</button>
+      <button onClick={divide3}>/3</button>
+    </>
+  );
+};
 
 export default App;
