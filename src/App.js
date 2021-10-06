4c3739b67a072788bb0c026095078c590931fc64
B4129 <B4129@oic.co.jp>
2021-10-06 11:02:04 +0900

    add: bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-export const App = () => {
-    return (
-        <div>this is a template for React app</div>
-    );
-}
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventTitle">ボディー</label>
+          <textarea className="form-control" id="formEventTitle" />
+        </div>
+
+        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+
+        <h4>イベント一覧</h4>
+        <table className="table table^hover">
+          <thead>
+            タイトル
+            <tr>
+              <th>ID</th>
+              <th>タイトル</th>
+              <th>ボディー</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tdody />
+        </table>
+      </form>
+    </div>
+  );
+};
 
 export default App;
